60a30dccade651acfaca13b5416dbc6907ce4e4a
dian <72053818+9yul@users.noreply.github.com>
2021-02-09 10:46:57 +0900

    이메일 보내기 업데이트

--- 이메일 보내기.py
+++ 이메일 보내기.py
@@ -1,9 +1,12 @@
 import smtplib
 from email.mime.text import MIMEText
+a = input("보낼 내용을 입력하세요:")
+b = input("제목을 입력하세요:")
+c = input("보낼 사람을 입력하세요:")
 s = smtplib.SMTP('smtp.gmail.com', 587)
 s.starttls()
-s.login('계정 이름', '계정 비밀번호')
-msg = MIMEText('내용 : 내용.')
-msg['Subject'] = '제목 : 제목.'
-s.sendmail("보내는 메일.", "받는 메일.", msg.as_string())
+s.login('메일 이름', '비밀번호')
+msg = MIMEText(a)
+msg['Subject'] = b
+s.sendmail("보낸 사람", c, msg.as_string())
 s.quit()
